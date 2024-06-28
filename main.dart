941bf24fc8f912b2534d63e6dbf1082c1631d072
dariomatias-dev <matiasdario75@gmail.com>
2024-06-28 14:41:15 -0300

    feat: created method to handle the result
    - Created method `handle` responsible for simplifying the process of dealing with the operation result.

--- main.dart
+++ main.dart
@@ -1,5 +1,25 @@
 abstract class Either<E extends Exception, S> {
   const Either();
+
+  T? handle<T>({
+    required T Function(S value) success,
+    required T Function(
+      FailureType type,
+      String message,
+    ) failure,
+  }) {
+    switch (this) {
+      case Success(value: var value):
+        return success(value);
+      case Failure(type: var type, message: var message):
+        return failure(
+          type,
+          message,
+        );
+      default:
+        return null;
+    }
+  }
 }
 
 class Success<S> extends Either<Exception, S> {
@@ -24,4 +44,13 @@ class Failure extends Either {
   final String message;
 }
 
-void main() {}
+void main() {
+  Either result = Success<String?>(
+    value: null,
+  );
+
+  result.handle(
+    success: (value) {},
+    failure: (type, message) {},
+  );
+}
