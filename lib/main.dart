2b198306a8ae1a82ae1d6b2a3b868290e78d08e4
dariomatias-dev <matiasdario75@gmail.com>
2024-06-28 19:25:36 -0300

    feat: added support for using environment variables
    - Installed Package: flutter_dotenv.
    - Added package initialization in the `main` file to use environment variables.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:flutter_error_handling/src/flutter_error_handling_app.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(
+    fileName: '.env',
+  );
+
   runApp(
     const FlutterErrorHandlingApp(),
   );
